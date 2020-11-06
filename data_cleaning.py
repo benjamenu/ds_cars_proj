81bb8492857f2c90d1c74a4eac8b0e227cc4dd7d
Benjamen Underwood <benjamenu@gmail.com>
2020-11-06 09:45:51 -0500

    Added some values for MSRP.

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,3 +8,7 @@ Created on Thu Nov  5 21:15:53 2020
 import pandas as pd
 
 df = pd.read_csv('cars.csv')
+df['MSRP'] = [29125, 35200, 39200, 29125,
+              28800, 28750, 25000, 59900,
+              63570, 73570, 73570, 27495,
+              28495, 
